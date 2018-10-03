62b94827d82a9c31bf2c5097edbeb5c863131c22
Bruce Mubangwa <40401943+brucetruth@users.noreply.github.com>
2018-10-03 18:38:18 +0200

    Create Functions.php
    Added simple math functions

---
+++ php-idea/utils/Functions.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'Timer.php';
+
+lass Distance {
+	function euclidean($point1, $point2){
+		$calc = 0;
+		$countPoint1 = count($point1);
+		$countPoint2 = count($point2);
+
+		if($countPoint1 == $countPoint2) {
+			for($x = 0; $x<$countPoint1; $x++) {
+				$calc += ($point2[$x] - $point1[$x])*($point2[$x] - $point1[$x]);
+			}
+		}
+
+		$calc = sqrt($calc);
+		return $calc;
+	}
+}
+
+class Functions {
+	function average($num1, $num2){
+		$avg = round(($num1/$num2)*100, 3) . "%";
+		return $avg;
+	}
+	function logistic($x) {
+		$fx = 1/(1+pow(2.7182, -1*($x)));
+		return $fx;
+	}
+}
+
+
+
+
+?>
