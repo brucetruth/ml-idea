96c67276aabb2057c76aad9aeb32fa85ec73513b
Bruce Truth <bruce@broosaction.com>
2026-02-12 19:14:55 +0200

    Add Execptions

---
+++ src/Exceptions/ModelNotTrainedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ML\IDEA\Exceptions;
+
+class ModelNotTrainedException extends \RuntimeException
+{
+}
---
+++ src/Exceptions/SerializationException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ML\IDEA\Exceptions;
+
+class SerializationException extends \RuntimeException
+{
+}
